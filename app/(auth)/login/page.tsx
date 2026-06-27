9cb26db54240c7beb2e595a49c1c65dcd834a44b
chapaiprakriti <prakriti9745906712@gmail.com>
2026-06-27 10:53:21 +0545

    Sprint 3 update login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 // app/(auth)/login/page.tsx
+app
 
 import LoginForm from "@/app/(auth)/_components/LoginForm";
 
 export default function Page() {
-    log
+
     return (
         <div>
             //div
